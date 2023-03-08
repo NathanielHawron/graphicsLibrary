b4ec8d24705077d63d5adfc49a6beb4a1607cbe5
NathanielHawron <nathanielhawron@gmail.com>
2023-03-07 23:17:42 -0800

    Added camera

---
+++ include/camera.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+
+namespace graphics{
+    class Camera{
+    private:
+        glm::vec3 pos;
+        float yaw, pitch, roll;
+        glm::mat4 projection;
+    public:
+        Camera(glm::vec3 pos, float yaw, float pitch, float roll);
+        ~Camera();
+        void setPos(glm::vec3 newPos);
+        void setYaw(float newYaw);
+        void setPitch(float newPitch);
+        void setRoll(float newRoll);
+        void updateProjectionMatrix(float FOV, float aspectRatio, float near = 0.1f, float far = 1000.0f);
+        glm::mat4 getViewMatrix();
+        glm::mat4 getProjectionMatrix();
+        glm::mat4 getViewProjMatrix();
+        glm::vec3 getFacingVector();
+    };
+}
