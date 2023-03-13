ffbcfd58a6c41a9ef4d185afb33aeb03f8bac259
NathanielHawron <nathanielhawron@gmail.com>
2023-03-13 13:50:40 -0700

    Added ways to get/modify camera position

--- include/camera.h
+++ include/camera.h
@@ -13,10 +13,12 @@ namespace graphics{
         Camera(glm::vec3 pos, float yaw, float pitch, float roll);
         ~Camera();
         void setPos(glm::vec3 newPos);
+        void addPos(glm::vec3 dPos);
         void setYaw(float newYaw);
         void setPitch(float newPitch);
         void setRoll(float newRoll);
         void updateProjectionMatrix(float FOV, float aspectRatio, float near = 0.1f, float far = 1000.0f);
+        const glm::vec3 *getPos();
         glm::mat4 getViewMatrix();
         glm::mat4 getProjectionMatrix();
         glm::mat4 getViewProjMatrix();
